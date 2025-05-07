0fba2025ac989da4ecfec3b6f36214954299bb46
Robert Phares <rjphares914@gmail.com>
2025-05-07 01:36:04 -0500

    initial commit

--- main.c
+++ main.c
@@ -1,61 +1,98 @@
 #include <SDL3/SDL.h>
 #include <glad/glad.h>
+#include <stdio.h>
+
+void reshape(int width, int height) {
+    glViewport(0, 0, width, height);
+    glLoadIdentity();
+    glOrtho(0.0, width, 0.0, height, -1.0, 1.0);
+}
 
 int main() {
-    // Initialize SDL
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        SDL_Log("Could not initialize SDL: %s", SDL_GetError());
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL Init failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create a window
-    SDL_Window *window = SDL_CreateWindow("SDL OpenGL Example", 800, 600, SDL_WINDOW_OPENGL);
-    if (!window) {
-        SDL_Log("Could not create window: %s", SDL_GetError());
-        SDL_Quit();
+    int w = 800, h = 800;;
+    SDL_Window* window = SDL_CreateWindow("OpenGL 2.1 + SDL3 (C)",
+                                          w, h,
+                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_GLContext context = SDL_GL_CreateContext(window);
+    if (!context) {
+        fprintf(stderr, "OpenGL context creation failed: %s\n", SDL_GetError());
         return 1;
     }
 
-    // Create an OpenGL context
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-    if (!glContext) {
-        SDL_Log("Could not create OpenGL context: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    // Initialize glad
     if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
-        SDL_Log("Failed to initialize GLAD");
-        SDL_GL_DestroyContext(glContext);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+        fprintf(stderr, "Failed to initialize GLAD\n");
         return 1;
     }
 
-    // Set the viewport
-    glViewport(0, 0, 800, 600);
+    printf("OpenGL version: %s\n", glGetString(GL_VERSION));
 
-    // Main loop
-    SDL_Event event;
+    // Set the initial orthographic projection to match the window size
+    reshape(w, h);
+
+    // if (glUniformMatrix4x3fv) {
+    //     printf("glUniformMatrix4x3fv is available. OpenGL 2.1 is supported.\n");
+    // } else {
+    //     printf("glUniformMatrix4x3fv is not available. OpenGL 2.1 is not supported.\n");
+    // }
+
+    // if (glSpecializeShader) {
+    //     printf("glSpecializeShader is available. OpenGL 4.6 is supported.\n");
+    // } else {
+    //     printf("glSpecializeShader is not available. OpenGL 4.6 is not supported.\n");
+    // }
+
+    //change ortho to 0,0,800,600
+    GLint mode;
+    glGetIntegerv(GL_MATRIX_MODE, &mode);
+    // Print the current matrix mode
+    if (mode == GL_MODELVIEW) {
+        printf("Current matrix mode: MODELVIEW\n");
+    } else if (mode == GL_PROJECTION) {
+        printf("Current matrix mode: PROJECTION\n");
+    } else if (mode == GL_TEXTURE) {
+        printf("Current matrix mode: TEXTURE\n");
+    } else if (mode == GL_COLOR) {
+        printf("Current matrix mode: COLOR\n");
+    } else {
+        printf("Unknown matrix mode: %d\n", mode);
+    }
+
     int running = 1;
+    SDL_Event e;
     while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_EVENT_QUIT) {
                 running = 0;
             }
+            if (e.type == SDL_EVENT_WINDOW_RESIZED) {
+                w = e.window.data1;
+                h = e.window.data2;
+                reshape(w, h);
+            }
         }
-
-        // Rendering code
-        glClearColor(0.1f, 0.2f, 0.3f, 1.0f); // Set clear color
-        glClear(GL_COLOR_BUFFER_BIT);        // Clear the screen
-
-        SDL_GL_SwapWindow(window);           // Swap buffers
+        glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_TRIANGLES);
+            glColor3f(1.0f, 0.0f, 0.0f);
+            for (int i = 0; i <= w/100; i++) {
+                glVertex2f(100.0*i, 0.0); // Bottom-left
+                glVertex2f(100.0*i + 50.0, h); // Middle-top
+                glVertex2f(100.0*i + 100, 0.0); // Bottom-right
+            }
+        glEnd();
+        SDL_GL_SwapWindow(window);
     }
 
-    // Clean up
-    SDL_GL_DestroyContext(glContext);
+    SDL_GL_DestroyContext(context);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
--- makefile
+++ makefile
@@ -1,2 +1,11 @@
+#add make file
+# This makefile is for compiling a simple SDL OpenGL program
+# is using mingw
+# and g++ compiler
+# It assumes that SDL2 and OpenGL are installed and properly configured
+# in your system
+# It also assumes that the SDL2 and OpenGL libraries are in the default library path
+# and the SDL2 include files are in the default include path
+
 make:
-	gcc main.c src/glad.c -o main -lSDL3
+	gcc main.c src/glad.c -o main -Iinclude -IC:/SDL3/x86_64-w64-mingw32/include -LC:/SDL3/x86_64-w64-mingw32/lib -lSDL3 -lopengl32
